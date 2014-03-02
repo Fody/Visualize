0a4a0e50f41131143f25c6d7efeda0f129d0eb9f
Simon Cropp <simon.cropp@gmail.com>
2014-03-02 11:27:59 +1100

    dev dep

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Visualize")]
 [assembly: AssemblyProduct("Visualize")]
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyFileVersion("0.2.3")]
+[assembly: AssemblyVersion("0.2.4")]
+[assembly: AssemblyFileVersion("0.2.4")]
