1bbaedce930d3c2e20906eb10ba054f39ddd2593
Cameron MacFarland <distantcam@gmail.com>
2014-03-18 19:45:17 +0800

    destroying powershell with xdt

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Visualize")]
 [assembly: AssemblyProduct("Visualize")]
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.4.0")]
+[assembly: AssemblyFileVersion("0.4.0")]
