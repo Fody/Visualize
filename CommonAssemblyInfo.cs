31093e9520b6a6823d2de86d91c11f768d412f71
Simon Cropp <simon.cropp@gmail.com>
2015-08-16 09:49:46 +1000

    fix nuspec name

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Visualize")]
 [assembly: AssemblyProduct("Visualize")]
-[assembly: AssemblyVersion("0.4.2")]
-[assembly: AssemblyFileVersion("0.4.2")]
+[assembly: AssemblyVersion("0.4.3")]
+[assembly: AssemblyFileVersion("0.4.3")]
